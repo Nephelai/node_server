4b8f280ad1d1aada859904eada9b11fd5ad5257c
Cheshire <jaeho406@gmail.com>
2018-06-25 16:42:47 +0900

    Get Client Ip(In route)

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -2,9 +2,20 @@ module.exports = function (app) {
     var index = require('../controllers/index.server.controller');
     app.get('/', index.render);
     app.get('/blog', function (req, res) {
-       res.render('../views/index.html');
-    });
-    app.get('/ob', function (req, res) {
-        res.render('../views/old_index.html');
+        var ip = req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress;
+
+        if (ip.length < 15) {
+            ip = ip;
+        }
+        else {
+            var nyIP = ip.slice(7);
+            ip = nyIP;
+        }
+
+        console.log(ip);
+        res.render('../views/index.html');
     });
 };
